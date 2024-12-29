8ac9c84feb8d7ffd04b6ad2233eed6e297792818
Nirmal <nirmalmhjr@gmail.com>
2024-12-29 11:57:40 +0545

    temporary token for testing, now removed

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,6 @@
 const conf = {
-    apiLoginUrl: process.env.REACT_APP_LOGIN_URL,
-    apiUrl: process.env.REACT_APP_API_URL,
-    temporaryToken: process.env.REACT_APP_TEMPORARY_TOKEN
-}
+  apiLoginUrl: process.env.REACT_APP_LOGIN_URL,
+  apiUrl: process.env.REACT_APP_API_URL,
+};
 
-export default conf
+export default conf;
